cba7840998ff848a40f6f9f2422c0d0a66c1e4bf
Dmitry Danilov <danilovdmitry94@gmail.com>
2017-11-02 16:52:49 +0200

    Added views

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Teacher, Discount, Course, CourseTypes, Student, CourseReview
+from .serializers import TeacherSerializer, DiscountSerializer, CourseSerializer, CourseTypesSerializer, StudentSerializer, CourseReviewSerializer
+
+
+class TeacherList(generics.ListAPIView):
+    """
+    API endpoint for listing Teacher objects
+    """
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+
+class CourseList(generics.ListAPIView):
+    """
+    API endpoint for listing Course objects
+    """
+    queryset = Course.objects.filter(is_active=True)
+    serializer_class = CourseSerializer
+
+
+class CourseTypesList(generics.ListAPIView):
+    """
+    API endpoint for listing CourseTypes objects
+    """
+    queryset = CourseTypes.objects.filter(is_active=True)
+    serializer_class = CourseTypesSerializer
+
+
+class StudentList(generics.ListCreateAPIView):
+    """
+    API endpoint for listing and creating Student objects
+    """
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class CourseReviewList(generics.ListCreateAPIView):
+    """
+    API endpoint for listing and creating CourseReview objects
+    """
+    queryset = CourseReview.objects.all()
+    serializer_class = CourseReviewSerializer
