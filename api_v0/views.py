2d43047da678cc414438b59b605c76df091fe9a6
Dmitry Danilov <danilovdmitry94@gmail.com>
2017-11-04 16:59:01 +0200

    Replaced views

--- api_v0/views.py
+++ api_v0/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets
 
-# Create your views here.
+from courses.models import Teacher, Discount, Course, CourseTypes, Student, CourseReview
+from .serializers import TeacherSerializer, DiscountSerializer, CoursePreviewSerializer, CourseDetailSerializer, CourseTypesSerializer, StudentSerializer, CourseReviewSerializer
+
+
+class TeacherList(generics.ListAPIView):
+    """
+    API endpoint for listing Teacher objects
+    """
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return CoursePreviewSerializer
+        return CourseDetailSerializer
+
+
+class CourseTypesList(generics.ListAPIView):
+    """
+    API endpoint for listing CourseTypes objects
+    """
+    queryset = CourseTypes.objects.filter(is_active=True)
+    serializer_class = CourseTypesSerializer
+
+
+class StudentList(generics.ListCreateAPIView):
+    """
+    API endpoint for listing and creating Student objects
+    """
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class CourseReviewList(generics.ListCreateAPIView):
+    """
+    API endpoint for listing and creating CourseReview objects
+    """
+    queryset = CourseReview.objects.all()
+    serializer_class = CourseReviewSerializer
