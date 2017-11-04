9d12c4534335f092a4f3362cf9b79ba409c0f93f
Dmitry Danilov <danilovdmitry94@gmail.com>
2017-11-04 22:18:45 +0200

    Added: ckeditor urls

--- filingroup/urls.py
+++ filingroup/urls.py
@@ -20,5 +20,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^api/v0/', include('api_v0.urls')),
     url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^ckeditor/', include('ckeditor_uploader.urls')),
     url(r'^admin/', admin.site.urls),
 ]
