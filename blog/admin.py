b21dfe082018f935d71c5c410e17e7ff9b919ecd
Dmitry Danilov <danilovdmitry94@gmail.com>
2017-11-04 21:33:13 +0200

    Added: admin integration

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
+from django.db.models import TextField
+from .models import Category, Tag, Post, Comment
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    radio_fields = {'is_active': admin.HORIZONTAL}
+    list_display = (
+        'title', 'is_active', 'date_added',
+    )
+    list_filter = [
+        'is_active', 'date_added',
+    ]
+
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    radio_fields = {'is_active': admin.HORIZONTAL}
+    list_display = (
+        'title', 'is_active', 'date_added',
+    )
+    list_filter = [
+        'is_active', 'date_added',
+    ]
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    radio_fields = {'is_published': admin.HORIZONTAL}
+    list_display = (
+        'title',
+        'is_post_published',
+        'date_added',
+    )
+    list_filter = [
+        'date_added',
+        'is_published',
+    ]
+    formfield_overrides = {TextField: {'widget': CKEditorUploadingWidget}, }
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'email',
+        'date_added',
+    )
+    list_filter = ['date_added']
+    formfield_overrides = {TextField: {'widget': CKEditorUploadingWidget},}
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
