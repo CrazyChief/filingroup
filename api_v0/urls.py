fff426c264af91c6bd4e27aa2cfc56f5bc5778ab
Dmitry Danilov <danilovdmitry94@gmail.com>
2017-11-04 22:09:10 +0200

    Added blog urls; Fixed: courses urls

--- api_v0/urls.py
+++ api_v0/urls.py
@@ -3,10 +3,18 @@ from .views import *
 
 
 router = DefaultRouter()
+
+#   courses routes
 router.register(r'courses', CourseViewSet)  #   route for Courses(list/detail)
-router.register(r'reviews', CourseReviewList)   #   route for reviews from students
-router.register(r'teachers', TeacherList)   #   route for teachers of course
-router.register(r'ctypes', CourseTypesList) #   route for course types (basic/all-inclusive/premium)
+router.register(r'reviews', CourseReviewViewSet)   #   route for reviews from students
+router.register(r'teachers', TeacherViewSet)   #   route for teachers of course
+router.register(r'ctypes', CourseTypesViewSet) #   route for course types (basic/all-inclusive/premium)
+
+#   blog routes
+router.register(r'categories', CategoryBlogViewSet) #   route for blog categories
+router.register(r'tags', TagViewSet)    #   route for blog tags
+router.register(r'posts', PostViewSet)  #   route for blog posts
+router.register(r'comments', CommentViewSet)    #   route for blog comments
 
 
 urlpatterns = router.urls
