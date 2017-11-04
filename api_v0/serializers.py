2b233063c7fa979d7ce37469f6acf0137435c45f
Dmitry Danilov <danilovdmitry94@gmail.com>
2017-11-04 21:52:01 +0200

    Added blog serializers

--- api_v0/serializers.py
+++ api_v0/serializers.py
@@ -1,53 +1,171 @@
 from rest_framework import serializers
 
 from courses.models import Teacher, Discount, Course, CourseTypes, Student, CourseReview
+from blog.models import Category, Tag, Post, Comment
 
 
+# Courses serializers
 class TeacherSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Teacher
-        fields = ('first_name', 'last_name', 'email', 'avatar', 'age', 'description')
+        fields = (
+            'first_name',
+            'last_name',
+            'email',
+            'avatar',
+            'age',
+            'description',
+        )
 
 
 class DiscountSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Discount
-        fields = ('title', 'slug', 'percents', 'from_date', 'till_date', 'author', 'created_at')
+        fields = (
+            'title',
+            'slug',
+            'percents',
+            'from_date',
+            'till_date',
+            'author',
+            'created_at',
+        )
 
 
 class CoursePreviewSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Course
-        fields = ('title', 'slug', 'discount', 'places', 'free_places', 'price')
+        fields = (
+            'title',
+            'slug',
+            'discount',
+            'places',
+            'free_places',
+            'price',
+        )
 
 
 class CourseDetailSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Course
-        fields = ('title', 'slug', 'discount', 'teachers', 'places', 'free_places', 'description', 'price', 'is_active', 'date_added')
+        fields = (
+            'title',
+            'slug',
+            'discount',
+            'teachers',
+            'places',
+            'free_places',
+            'description',
+            'price',
+            'is_active',
+            'date_added',
+        )
 
 
 class CourseTypesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CourseTypes
-        fields = ('permission', 'title', 'course', 'is_active', 'description')
+        fields = (
+            'permission',
+            'title',
+            'course',
+            'is_active',
+            'description',
+        )
 
 
 class StudentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Student
-        fields = ('first_name', 'last_name', 'email', 'phone', 'skype', 'courses', 'date_added')
+        fields = (
+            'first_name',
+            'last_name',
+            'email',
+            'phone',
+            'skype',
+            'courses',
+            'date_added',
+        )
 
 
 class CourseReviewSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CourseReview
-        fields = ('course', 'first_name', 'last_name', 'avatar', 'link_to_fb', 'link_to_inst', 'link_to_linkedin', 'review', 'video', 'date_added')
+        fields = (
+            'course',
+            'first_name',
+            'last_name',
+            'avatar',
+            'link_to_fb',
+            'link_to_inst',
+            'link_to_linkedin',
+            'review',
+            'video',
+            'date_added',
+        )
+
+
+# Blog serializers
+class CategoryBlogSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = (
+            'title',
+            'slug',
+            'is_active',
+            'date_added',
+        )
+
+
+class TagSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tag
+        fields = (
+            'title',
+            'slug',
+            'is_active',
+            'date_added',
+        )
+
+
+class PostSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Post
+        fields = (
+            'title',
+            'slug',
+            'cover_picture',
+            'category',
+            'is_published',
+            'announce', 'content',
+            'tags',
+            'author',
+            'date_added',
+        )
+        lookup_field = 'slug'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = (
+            'post',
+            'name',
+            'email',
+            'text',
+            'parent',
+            'user',
+            'date_added',
+        )
 
