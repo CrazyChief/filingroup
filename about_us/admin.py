e6a89f08893bc052f3dea382c8751fe2fb138e00
Dmitry Danilov <danilovdmitry94@gmail.com>
2017-11-06 19:03:09 +0200

    Added: model, admin integration, migrations

--- about_us/admin.py
+++ about_us/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.db.models import TextField
+from .models import AboutModel
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
-# Register your models here.
+
+class AboutAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 'date_added', 'date_updated',
+    )
+    list_filter = [
+        'date_added', 'date_updated',
+    ]
+    formfield_overrides = {TextField: {'widget': CKEditorUploadingWidget}, }
+
+
+admin.site.register(AboutModel, AboutAdmin)
--- about_us/models.py
+++ about_us/models.py
@@ -1,3 +1,30 @@
+from django.utils.translation import ugettext_lazy as _
 from django.db import models
 
-# Create your models here.
+from courses.models import Course
+
+
+def upload_path(instance, filename):
+    """
+    Path for about model where files are stored
+    :param instance:
+    :param filename:
+    :return:
+    """
+    return "about_us/{0}".format(filename)
+
+
+class AboutModel(models.Model):
+    title = models.CharField(max_length=100, verbose_name=_('Type of content (first try, etc.)'))
+    logo = models.FileField(upload_to=upload_path, verbose_name=_('About Us'))
+    is_active = models.BooleanField(default=False, verbose_name=_('Is About description active?'))
+    text = models.TextField(verbose_name=_('Text'))
+    date_added = models.DateTimeField(auto_now_add=True, verbose_name=_('Date added'))
+    date_updated = models.DateTimeField(auto_now=True, verbose_name=_('Date updated'))
+
+    class Meta:
+        verbose_name = _('About Us')
+        verbose_name_plural = _('About Us')
+
+    def __str__(self):
+        return str(self.title)
