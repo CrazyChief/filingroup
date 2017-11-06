d8c3a5dcd698395244e3fbe1a606d8964b0cee6c
gloompi <gloompi@gmail.com>
2017-11-06 21:19:34 +0800

    homepage is done

--- frontend/src/components/HomeSections/FifthSection.js
+++ frontend/src/components/HomeSections/FifthSection.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import FontAwesome from 'react-fontawesome'
 
 import ParticledElement from 'components/ParticledElement'
 
 export default function FifthSection() {
   return(
-    <section className="section home__fifth-section">
-      <ParticledElement />
+    <section className="home__fifth-section">
+      <ParticledElement height="100%" />
       <div className="container home__fifth-container">
         <div className="macbook__pic">
           <img src="assets/img/main/macbook.png" alt="macbook"/>
+          <a href="#" className="get__link banner__get-link">
+            <FontAwesome name="play-circle-o" />
+            Получить доступ к видео
+          </a>
         </div>
+        <h2 className="home__fifth-title bold">
+          Посмотрите видео о том, как найти 
+          <span className="orange"> товар для успешного </span>
+           старта на <img src="assets/img/main/arrow.png" alt="amazon"/>
+        </h2>
       </div>
     </section>
   )
