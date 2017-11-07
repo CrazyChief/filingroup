4c2a444a53cff76ac28b2218b456c9efe3500d5e
Dmitry Danilov <danilovdmitry94@gmail.com>
2017-11-07 14:57:15 +0200

    Added: SiteRule model

--- site_rules/models.py
+++ site_rules/models.py
@@ -1,3 +1,18 @@
+from django.utils.translation import ugettext_lazy as _
 from django.db import models
 
-# Create your models here.
+
+class SiteRule(models.Model):
+    title = models.CharField(max_length=100, verbose_name=_('Version of site rules'))
+    is_active = models.BooleanField(default=False, verbose_name=_('Is this version active?'))
+    content = models.TextField(verbose_name=_('Content'))
+    date_added = models.DateTimeField(auto_now_add=True, verbose_name=_('Date added'))
+    date_updated = models.DateTimeField(auto_now=True, verbose_name=_('Date updated'))
+
+    class Meta:
+        ordering = ['-date_added']
+        verbose_name = _('Site Rule')
+        verbose_name_plural = _('Site Rules')
+
+    def __str__(self):
+        return str(self.title)
