31d285c64a2f2df219f87a2600f0e810baa55672
Dmitry Danilov <danilovdmitry94@gmail.com>
2017-11-07 15:49:01 +0200

    Added: admin integration

--- denial/admin.py
+++ denial/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.db.models import TextField
 
-# Register your models here.
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from .models import Denial
+
+
+class DenialAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 'is_active', 'date_added', 'date_updated',
+    )
+    list_filter = [
+        'is_active', 'date_added', 'date_updated',
+    ]
+    formfield_overrides = {TextField: {'widget': CKEditorUploadingWidget}}
+
+
+admin.site.register(Denial, DenialAdmin)
