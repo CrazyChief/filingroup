ea2a8e018268beac0530e8e1189f7e459da061a2
Dmitry Danilov <danilovdmitry94@gmail.com>
2017-11-13 15:38:22 +0200

    Swiched: database from sqlite3 to postgresql

--- filingroup/settings.py
+++ filingroup/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'filingroup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'filingroup',
+        'USER': 'postgres',
+        'PASSWORD': 'NumberOne123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
