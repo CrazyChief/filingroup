d8b203eebd6eb631b718b14f0b13c63840e23904
Dmitry Danilov <danilovdmitry94@gmail.com>
2017-11-21 23:07:15 +0200

    Fixed: template and urls

--- filingroup/urls.py
+++ filingroup/urls.py
@@ -21,7 +21,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^api/v0/', include('api_v0.urls')),
-    url(r'^', TemplateView.as_view(template_name='index.html')),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
     url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
     url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
