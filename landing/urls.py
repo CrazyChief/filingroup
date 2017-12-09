7385e44da142f3243673e8234af58b44766be242
Dmitry Danilov <danilovdmitry94@gmail.com>
2017-12-09 19:00:37 +0200

    Updated urls

--- landing/urls.py
+++ landing/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import LandingView
+from .views import LandingView, ThanksView
 
 
 urlpatterns = [
     url(r'^(?P<slug>[-\w]+)([/]+|([^.*/]+))$', LandingView.as_view()),
+    url(r'^(?P<slug>[-\w]+)/thanks/$', ThanksView.as_view(), name='thanks'),
 ]
 
