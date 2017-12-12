eda52546a9df44afc29ac68eb88a1c2017f7b280
Dmitry Danilov <danilovdmitry94@gmail.com>
2017-12-12 16:07:35 +0200

    Fixed urls

--- landing/urls.py
+++ landing/urls.py
@@ -5,6 +5,6 @@ app_name = 'landings'
 urlpatterns = [
     url(r'^(?P<slug>[-\w]+)([/]+|([^.*/]+))$', LandingView.as_view(), name='land_view'),
     url(r'^(?P<slug>[-\w]+)/thanks/$', ThanksView.as_view(), name='thanks'),
-    url(r'^/error/$', ErrorView.as_view(), name='error'),
+    url(r'^error/$', ErrorView.as_view(), name='error'),
 ]
 
